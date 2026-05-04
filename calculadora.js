0c91131a80a9f4b8d2ff48b67bc1b96c521e16e0
Valentino Mantilla <valentinomantilla@MacBook-Air-de-Valentino-2.local>
2026-05-04 12:38:00 -0500

    Concatenar resultados en suma

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,7 @@
 let numeroTexto = ""
 let numeroGuardado = 0
 let operacionRealizada = ""
+
 function numeroOnclick(numero){
     console.log(numero)
     const cajaTexto = document.getElementById("texto")
@@ -8,21 +9,53 @@ function numeroOnclick(numero){
     cajaTexto.value = numeroTexto
 }
 
-function sumarOnClick(){
+function sumarOnClick(){ 
+    numeroGuardado = numeroGuardado + parseInt(numeroTexto) 
+    numeroTexto = "" 
+    const cajaTexto = document.getElementById("texto") 
+    cajaTexto.value = "" 
+    operacionRealizada = "+" 
+}
+
+function restarOnClick(){
+    numeroGuardado = numeroGuardado - parseInt(numeroTexto)
+    numeroTexto = ""
+    const cajaTexto = document.getElementById("texto")
+    cajaTexto.value = ""
+    operacionRealizada = "-"
+}
+
+function multiOnClick(){
     numeroGuardado = numeroTexto
     numeroTexto = ""
     const cajaTexto = document.getElementById("texto")
     cajaTexto.value = ""
-    operacionRealizada = "+"
+    operacionRealizada = "*"
 }
 
-function igualOnClick(){
-    const cajaTexto = document.getElementById("texto")
-    if (operacionRealizada === "+"){
-        const resultado = parseInt(numeroGuardado) + parseInt(numeroTexto)
-        cajaTexto.value = resultado
-    }
-    numeroGuardado = ""
+function COnClick(){
+    numeroGuardado = 0
     numeroTexto = ""
     operacionRealizada = ""
+    const cajaTexto = document.getElementById("texto")
+    cajaTexto.value = ""
+}
+
+function igualOnClick(){ 
+    const cajaTexto = document.getElementById("texto") 
+    if (operacionRealizada === "+"){ 
+        const resultado = parseInt(numeroGuardado) + parseInt(numeroTexto) 
+        cajaTexto.value = resultado 
+    } 
+    if (operacionRealizada === "-"){ 
+        const resultado = parseInt(numeroGuardado) - parseInt(numeroTexto) 
+        cajaTexto.value = resultado 
+    } 
+    if (operacionRealizada === "*"){ 
+        const resultado = parseInt(numeroGuardado) * parseInt(numeroTexto) 
+        cajaTexto.value = resultado 
+    } 
+    numeroGuardado = resultado 
+    numeroTexto = "" 
+    operacionRealizada = "" 
 }
